rotations, correct value backprop, no-res alphanet

alphaNet.py
@@ -17,9 +17,9 @@ class ConvBlock(nn.Module):
     (3) A rectifier nonlinearity" (AlphaGo supplement)
     256 filters has been changed to a lower number simply because tictactoe is way simpler than Go"
     """
-    def __init__(self, board_layers=3, out_filters=32):
+    def __init__(self, in_filters=3, out_filters=32):
         super(ConvBlock,self).__init__()
-        self.conv1 = nn.Conv2d(board_layers, out_filters, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(in_filters, out_filters, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(out_filters)
         # relu
     
@@ -104,15 +104,17 @@ class ValueHead(nn.Module):
         self.bn1 = nn.BatchNorm2d(1)
         # relu
         self.fc1 = nn.Linear(self.board_size*1, hidden_dims)
+        self.dropout1 = nn.Dropout(.3)
         # relu
         self.fc2 = nn.Linear(hidden_dims, 1)
+        self.dropout2 = nn.Dropout(.3)
         # tanh
     
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
         x = x.view(-1, self.board_size*1)
-        x = F.relu(self.fc1(x))
-        x = torch.tanh(self.fc2(x))
+        x = self.dropout1(F.relu(self.fc1(x)))
+        x = self.dropout2(torch.tanh(self.fc2(x)))
         return x
 
 class AlphaNet(nn.Module):
@@ -125,7 +127,7 @@ class AlphaNet(nn.Module):
     """
     def __init__(self, board_layers=0, board_size=0, action_space=0, n_filters=0, n_hidden=0, n_res=0):
         super(AlphaNet, self).__init__()
-        self.conv_block1 = ConvBlock(board_layers=board_layers, out_filters=n_filters)
+        self.conv_block1 = ConvBlock(in_filters=board_layers, out_filters=n_filters)
         
         self.n_res = n_res
         for k in range(n_res):
@@ -145,6 +147,48 @@ class AlphaNet(nn.Module):
     
     def load_weights(self, weights_filepath):
         self.load_state_dict(torch.load(weights_filepath))
+
+class NoResAlphaNet(nn.Module):
+    def __init__(self, board_layers=0, board_size=0, action_space=0, n_filters=0, n_hidden=0, n_conv=0):
+        super(NoResAlphaNet, self).__init__()
+        
+        self.n_conv = n_conv
+        for k in range(self.n_conv):
+            setattr(self, f"conv_block{k+1}", ConvBlock(in_filters=(board_layers if k==0 else n_filters), out_filters=n_filters))
+
+        self.conv_out = n_filters*9 # TIC TAC TOE ONLY
+        self.fc1 = nn.Linear(self.conv_out, n_hidden)
+        self.dropout1 = nn.Dropout(.3)
+        # relu
+
+        self.fc2 = nn.Linear(n_hidden, n_hidden//2)
+        self.dropout2 = nn.Dropout(.3)
+        # relu
+
+        self.policy_head = nn.Linear(n_hidden//2, action_space)
+        self.softmax = nn.LogSoftmax(dim=-1)
+
+        self.value_head = nn.Linear(n_hidden//2, 1)
+        # tanh
+    
+    def forward(self, x):
+        for k in range(self.n_conv):
+            x = getattr(self, f"conv_block{k+1}")(x)
+
+        x = x.view(-1, self.conv_out)
+        x = F.relu(self.dropout1(self.fc1(x)))
+        x = F.relu(self.dropout2(self.fc2(x)))
+
+        p = self.policy_head(x)
+        p = self.softmax(p)
+
+        v = self.value_head(x).squeeze(-1)
+        v = torch.tanh(v)
+        return p,v
+    
+    def load_weights(self, weights_filepath):
+        self.load_state_dict(torch.load(weights_filepath))
+        
 
 class AlphaLoss(nn.Module):
     def __init__(self):

alphaZero.py
@@ -4,17 +4,28 @@ import network
 import alphaNet
 import board
 from utils import *
+import torch
+from tqdm import tqdm
+import os
 
 class AlphaZero:
     def __init__(self):
+
+        # cleanup old files
+        for dirfile in os.listdir('net_files'):
+            if "temp" in dirfile:
+                os.remove(f'net_files/{dirfile}')
+
         self.game_board = board.T3Board
+        storage_buffer_len = int(5e3)
         
         net_params = {
             'board_layers': self.game_board.encoding_layers,
             'board_size': self.game_board.board_size,
             'action_space': self.game_board.action_space,
-            'n_filters': 512, 
-            'n_hidden': 512, 
+            'n_filters': 64, #64
+            'n_hidden': 64, #64
+            # 'n_conv': 4
             'n_res': 2
         }
 
@@ -23,46 +34,68 @@ class AlphaZero:
                 'net_params': net_params, 
                 'loss_cls': alphaNet.AlphaLoss, 
                 'optim_lr': 1e-3,
-                'weight_decay': .1,
+                'weight_decay': 0,
                 'weights_filepath': None,
-                'n_epochs': 5,
+                'n_epochs': 10,
                 'bsz': 64
         }
 
         mcts_kwargs = {
-            'n_games': 25,
+            'n_games': 5,
             'n_sims_per_game_step': 25,
-            'c_puct_decay_n': 10
         }
 
         net_config = Config(**net_kwargs)
         mcts_config = Config(**mcts_kwargs)
-        self.storage = Storage(net_config, mcts_config, self.game_board, buffer_len=int(1e5))
+        self.storage = Storage(net_config, mcts_config, self.game_board, buffer_len=storage_buffer_len)
+
+        self.n_display_games = 2
+        
 
     def alpha_zero(self):
         net = self.storage.latest_network()
+        self.storage.save_network(net)
+        print(net)
         loops = 0
         while True:
+            net = self.storage.latest_network()
             try:
-                print(f'\x1b[1;30;41mALPHAZERO LOOP {loops+1}\x1b[0m')
+                print(f'\x1b[1;30;41m ALPHAZERO LOOP {loops+1} \x1b[0m')
                 mcts.self_play(self.storage)
                 net.train_epochs(self.storage)
-                if loops % 10 == 0:
-                    for k in range(5):
+                if self.compare_networks([self.storage.latest_network(), net]):
+                    self.storage.save_network(net)
+                if (loops+1) % 10 == 0:
+                    for k in range(self.n_display_games):
                         print(f'Game {k+1}')
-                        self.self_play_game(self.storage.latest_network())
+                        self.self_play_game([self.storage.latest_network(),self.storage.latest_network()], print_game=True)
                 loops += 1
             except KeyboardInterrupt:
                 return self.storage.latest_network()
 
-    def self_play_game(self, network):
+    # networks = [old_net, new_net]
+    def compare_networks(self, networks, games=10):
+        new_wins = []
+        for k in tqdm(range(games), desc='Comparing Networks'):
+            turn_winner = self.self_play_game(networks[::(-1)**k], print_game=(k==0))
+            # 1 point to new net for winning, .5 points for draw, 0 points for losing
+            new_wins.append( [.5, 0, 1][ turn_winner*((-1)**k) ] )
+        print(f"Results: {new_wins}\nNew winrate: {sum(new_wins)/games}") 
+        return sum(new_wins)/games >= .55
+
+    def self_play_game(self, networks, print_game=False):
         game_board = self.game_board()
-        while game_board.end_state() == 0:
-            action = mcts.play_learned_action(network, game_board, n_sim=100)
+        turn = 0
+        while game_board.end_state() is None:
+            action = mcts.play_learned_action(networks[turn%2], game_board, n_sim=25, print_state=print_game)
             game_board.play(action)
-            print(game_board)
+            if print_game:
+                print(game_board)
+        return game_board.end_state()
 
 
 if __name__ == '__main__':
     az = AlphaZero()
-    az.alpha_zero()
+    final_net = az.alpha_zero()
+    save_file = input('Save as? (ctrl-c to quit without saving) ')
+    torch.save(final_net.net.state_dict(), f'net_files/final/{save_file}.pth')

board.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 class Board(object):
     def __init__(self, history):
         self.history = history or []
-        self.player = [1,-1][len(history)%2] if history else 1
+        self.player = [1,-1][len(history)%2] if history is not None else 1
         self.board = None
 
     def play(self, action):
@@ -34,7 +34,7 @@ class Board(object):
 class T3Board(Board):
     action_space = 9
     board_size = 9
-    encoding_layers = 3
+    encoding_layers = 1
 
     def __init__(self, history=None, board=None):
         super(T3Board,self).__init__(history)
@@ -66,8 +66,8 @@ class T3Board(Board):
 
         # draw checking
         if not (self.board == 0).any():
-            return 0.5
-        return 0
+            return 0
+        return None
 
     """
     "The M feature planes are composed of binary feature planes indicating the presence of the player’s pieces, 
@@ -81,34 +81,41 @@ class T3Board(Board):
     is "fully observable soley from the current [board state]" (AlphaGo supplement)
     """
     def encoded(self):
-        encoded = np.zeros((3,3,3), dtype=np.float32)
+        encoded = np.zeros((self.__class__.encoding_layers,3,3), dtype=np.float32)
         # M planes
-        encoded[:,:,0] = (self.board==1) # O's
-        encoded[:,:,1] = (self.board==-1) # X's
+        # encoded[:,:,0] = (self.board==1) # O's
+        # encoded[:,:,1] = (self.board==-1) # X's
+        
+        # encoded[0,:,:] = (self.board == self.player)
+        # encoded[1,:,:] = (self.board == -self.player)
+        
+        encoded[0,:,:] = self.board*self.player
 
         # L planes
-        encoded[:,:,2] = self.player # current player (L1)
+        # encoded[2,:,:] = self.player # current player (L1)
         return encoded
-
-    def legal_actions(self):
-        x,y = np.where(self.board == 0)
-        actions = list(zip(list(x),list(y)))
-        return actions
-
-    def actions_to_idxs(self, actions):
-        mapping = {}
-        for action in actions:
-            mapping[action] = action[0] * 3+action[1]
-        return mapping
     
-    def idxs_to_actions(self, idxs):
-        mapping = {}
-        for idx in idxs:
-            mapping[idx] = (idx//3, idx%3) 
+    def rotations(self, pi):
+        board = self.encoded()
+        pi = pi.reshape(3,3)
+        stack = np.insert(board,1,pi,axis=0)
+        out = []
+        stack_to_tuple = lambda arr: (arr[:-1], arr[-1].reshape(-1))
+        for k in range(4):
+            stack = np.rot90(stack, k=k, axes=(-1,-2))
+            out += [stack_to_tuple(stack), stack_to_tuple(np.fliplr(stack))]
+        return out
+
+    # returns a boolean array of length action_space
+    def legal_actions(self):
+        return self.board.reshape(-1) == 0
+    
+    def idx_action_map(self):
+        mapping = [(k//3, k%3) for k in range(self.__class__.action_space)]
         return mapping
 
     def clone(self):
-        return T3Board(history=self.history, board=np.copy(self.board))
+        return T3Board(history=list(self.history), board=np.copy(self.board))
 
     def __str__(self):
         str_player = ['X','O'][int(self.player/2+.5)]
@@ -116,7 +123,7 @@ class T3Board(Board):
         str_board = '\n'.join(['|'.join(str_board[k]) for k in range(len(str_board))])
         str_board = str_board.replace('-1','X').replace('1','O').replace('0',' ')
         is_won = self.end_state()
-        if is_won == 0:
+        if is_won is None:
             return f"\n{str_board}\n{str_player}'s turn"
         out_state_string = ['Draw','Player O Wins','Player X Wins'][int(is_won)]
         return f"\n{str_board}\n{out_state_string}"

mcts.py
@@ -17,37 +17,43 @@ class Node(object):
         self.q = 0
         self.parent = parent
         self.action = action
-        self.children = {}
+        self.children = []
         self.is_expanded = False
     
-    def expand(self, action_dict):
-        for action, p in action_dict.items():
-            self.children[action] = Node(p=p, parent=self, action=action)
+    def expand(self, node_params):
+        for p, action in node_params:
+            # print(f"action={action}, p={p}")
+            self.children.append( Node(p=p, parent=self, action=action) ) 
         self.is_expanded = True
     
     def update(self, value):
         self.n += 1
         self.w += value
         self.q = self.w/self.n
+    
+    def __str__(self):
+        return f"action={self.action}, p={self.p}, q={self.q}, n={self.n}, w={self.w}"
 
 def select_child(node, c_puct=1):
         # print(node.children)
-        actions, N, Q, P = zip(*[ [child.action, child.n, child.q, child.p] for child in node.children.values() ])
-        N, Q, P = map(np.array, (N,Q,P))
+        N, Q, P = map( np.array, zip(*[ [child.n, child.q, child.p] for child in node.children ]) )
+        # print(P)
         U = c_puct * P * np.sqrt(np.sum(N)) / (1+N)
-        best_action = actions[np.argmax(Q+U)]
-        return node.children[best_action]
+        # print(f"Q={Q}\nU={U}\nQ+U={Q+U}")
+        return node.children[np.argmax(Q+U)]
 
 def select_leaf(root, c_puct):
     node = root
     search_path = [root]
     while node.is_expanded:
-        node = select_child(node, c_puct=1)
+        node = select_child(node, c_puct=c_puct)
+        # print(node)
         search_path.append(node)
     return search_path
 
 def evaluate(network, board):
     # NEED TO DO BOARD ROTATIONS N STUFF SOMEWHERE
+    network.eval()
     encoded = board.encoded()
     encoded = np.expand_dims(encoded, axis=0)
     pi, v = network.predict(encoded)
@@ -57,45 +63,68 @@ def backup(leaf, v):
     node = leaf
     while node.parent is not None:
         node.update(v)
+        # print(node)
         v *= -1
         node = node.parent
     # root
     node.update(v)
 
 def add_dirichlet_noise(root):
-    child_ps = [ [action, child.p] for action, child in root.children.items()]
-    noise_distrib = np.random.gamma(.03, 1, len(root.children))
-    for k in range(len(child_ps)):
-        child_ps[k][1] = .75*child_ps[k][1] + .25*noise_distrib[k]
-    for action, child_p in child_ps:
-        root.children[action].p = child_p
+    child_ps = np.array([child.p for child in root.children])
+    noise_distrib = np.random.gamma(.1, 1, len(root.children))
+    child_ps = .75*child_ps + .25*noise_distrib
+    for k, child_p in enumerate(child_ps):
+        root.children[k].p = child_p
     return root
 
-def search(board, network, n_sim=100, c_puct=1, learning=True):
-    network.eval()
+def search(board, network, n_sim=100, c_puct=1, add_noise=True):
     root = Node()
+    default_prob = board.legal_actions().astype(float)
+    default_prob /= np.sum(default_prob)
+    default = zip(default_prob, board.idx_action_map())
+    root.expand(default)
+    if add_noise:
+        root = add_dirichlet_noise(root)
+    # print(np.array([child.p for child in root.children]))
+    
     for _ in range(n_sim):
-        if root.is_expanded and learning:
-            root = add_dirichlet_noise(root)
+        temp_board = board.clone()
+        # print("SELECT")
         search_path = select_leaf(root, c_puct) # select
         leaf = search_path[-1]
         for node in search_path[1:]:
-            board.play(node.action)
-        pi, v = evaluate(network, board) # evaluate
-        legal_actions = board.legal_actions()
-        legal_action_idxs = board.actions_to_idxs(legal_actions)
-        action_dict = {a:pi[ legal_action_idxs[a] ] for a in legal_actions}
-        if len(action_dict) > 0:
-            leaf.expand(action_dict)
-        backup(leaf, v)
+            temp_board.play(node.action)
+        # print(temp_board)
+        # print("EVAL")
+        pi, v = evaluate(network, temp_board) # evaluate
+
+        action_ps = pi * temp_board.legal_actions()
+        if sum(action_ps) > 0:
+            action_ps /= sum(action_ps)
+            node_params = list(zip(action_ps, board.idx_action_map()))
+            leaf.expand(node_params) # expand
+            backup(leaf, -v) # backup
+        else:
+            z = temp_board.end_state()
+            # if temp_board.player != board.player:
+            #     z = -z
+            backup(leaf, -z*board.player)
+    # print(board)
+    # print(board.player, [(child.q, child.n, child.w) for child in root.children])
     return root
 
-def get_pi(node, tau):
-    actions, N = zip(*[ [child.action, child.n] for child in node.children.values() ])
-    N = np.array(N)
+def get_pi(node, tau, mask):
+    N = np.array([child.n for child in node.children])
+    # print(N)
+    N *= mask
+    # print(N)
+    if tau == 0:
+        pi = np.zeros(N.shape)
+        pi[np.argmax(N)] = 1
+        return pi
     pi = N**(1/tau)
     pi /= np.sum(pi)
-    return list(actions), pi
+    return pi
 
 def self_play(storage):
     mcts_config = storage.mcts_config
@@ -103,43 +132,46 @@ def self_play(storage):
     dataset = []
     for _ in trange(mcts_config.n_games, desc='MCTS Data Collection'):
         tau = 1
+        c_puct = .5
         board = storage.board_cls()
         s_arr = []
         pi_arr = []
-        v = 0
+        v_arr = []
+        z = None
         plays = 0
-        while v == 0:
-            c_puct = 1 if plays < mcts_config.c_puct_decay_n else 1e-3
-            # ROTATIONS ARE MISSING
-            s_arr.append(board.encoded())
-            node = search(board.clone(), network, n_sim=mcts_config.n_sims_per_game_step, c_puct=c_puct, learning=True)
+        while z is None:
 
-            actions, pi = get_pi(node, tau)
+            # ROTATIONS ARE MISSING
+            node = search(board, network, n_sim=mcts_config.n_sims_per_game_step, c_puct=c_puct, add_noise=True)
+
+            pi = get_pi(node, tau, mask=board.legal_actions())
+            for b, p in board.rotations(pi):
+                s_arr.append(b)
+                pi_arr.append(p)
+                v_arr.append(board.player)
+            
             action_idx = np.random.choice(range(len(pi)),p=pi)
-            action = actions[action_idx]
-            
-            pi_temp = np.zeros(storage.board_cls.action_space)
-            legal_action_idxs = board.actions_to_idxs(actions)
-            for k in range(len(actions)):
-                pi_temp[ legal_action_idxs[actions[k]] ] = pi[k]
-            pi_arr.append(pi_temp)
-            
+            action = board.idx_action_map()[action_idx]
             board.play(action)
             plays += 1
             
-            v = board.end_state()
-        if v**2 < 1:
-            v_arr = [.5]*len(pi_arr) 
-        else:
-            v_arr = np.array( [(-1)**(k%2) for k in range(len(pi_arr))] )
-            if v == -1:
-                v_arr = -v_arr 
+            z = board.end_state()
+        
+        v_arr = z * np.array(v_arr).astype(float)
+        # if z == 0:
+        #     v_arr += .5
         dataset += list(zip(s_arr, pi_arr, v_arr))
+        # print(dataset[-2:])
     storage.save_sub_dataset(dataset)
 
-def play_learned_action(network, board, n_sim=100):
-    node = search(board.clone(), network, n_sim=100, c_puct=1e-3, learning=False)
-    actions, pi = get_pi(node, 1)
+def play_learned_action(network, board, n_sim=25, print_state=False):
+    node = search(board, network, n_sim=n_sim, c_puct=.5, add_noise=True)
+    pi = get_pi(node, tau=1, mask=board.legal_actions())
+    funky, board_value = evaluate(network, board)
+    actions = board.idx_action_map()
+    if print_state:
+        print("VALUE:", board_value)
+        print(list(zip(actions,pi,funky)))
     act_idx = np.argmax(pi)
     return actions[act_idx]
     

network.py
@@ -12,11 +12,11 @@ class Network(object):
         if net_config.weights_filepath is not None:
             self.net.load_weights(net_config.weights_filepath)
         self.loss_func = net_config.loss_cls()
+        self.optimizer = torch.optim.Adam(self.net.parameters(), lr=net_config.optim_lr, weight_decay=net_config.weight_decay)
         try:
             self.loss_func = net_config.loss_cls().eval()
         except:
             pass
-        self.optimizer = torch.optim.Adam(self.net.parameters(), lr=net_config.optim_lr, weight_decay=net_config.weight_decay)
     
     # runs 1 epoch of training
     def train_epochs(self, storage):
@@ -46,11 +46,14 @@ class Network(object):
         p, v = self.net(inp)
         v = v.detach().numpy().squeeze()
         p = p.detach().exp().numpy().squeeze()
-        pi = {k:p[k] for k in range(len(p))}
-        return pi, v
+        # print("PRED_V="+str(v))
+        return p, v
     
     def train(self):
         self.net.train()
 
     def eval(self):
         self.net.eval()
+    
+    def __str__(self):
+        return str(self.net)

storage.py
@@ -18,7 +18,7 @@ class Storage(object):
         net = network.Network(self.net_config)
         if len(self.networks) > 0:
             self.net_config.weights_filepath = self.networks[-1]
-            net.load_state_dict(torch.load(self.net_config.weights_filepath))
+            net.net.load_state_dict(torch.load(self.net_config.weights_filepath))
         return net
     
     def save_network(self, network):
@@ -28,11 +28,13 @@ class Storage(object):
         if len(self.networks) < 5:
             self.networks.append(filename)
         else:
-            self.networks = networks[1:]+[filename]
+            self.networks = self.networks[1:]+[filename]
+        print(f'Saved networks: {self.n_networks}')
     
     def save_sub_dataset(self, dataset):
         removable_idxs = max(0, len(self.dataset)+len(dataset)-self.dataset_buffer_len)
         self.dataset = self.dataset[removable_idxs:] + dataset
+        print(f'Current dataset length: {len(self.dataset)}')
     
     def get_batches(self):
         data_idxs = np.array(range(len(self.dataset)))